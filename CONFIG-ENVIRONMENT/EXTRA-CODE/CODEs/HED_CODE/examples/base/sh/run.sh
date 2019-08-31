8fcdbcb1f0a0d2e6d80abc879c41e256634c7834
Tiago Santos <santos.t.j.dos@gmail.com>
2019-08-30 23:40:41 -0300

    Exec by bash

--- CONFIG-ENVIRONMENT/EXTRA-CODE/CODEs/HED_CODE/examples/base/sh/run.sh
+++ CONFIG-ENVIRONMENT/EXTRA-CODE/CODEs/HED_CODE/examples/base/sh/run.sh
@@ -1,6 +1,6 @@
 #https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file
 if [ $# -eq 0 ]; then
-    ./hed.sh |& tee hed.log
+    bash ./hed.sh |& tee hed.log
 else
-    ./hed.sh |& tee $1
+    bash ./hed.sh |& tee $1
 fi
